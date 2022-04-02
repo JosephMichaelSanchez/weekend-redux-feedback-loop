6ee68ff2aad05959a62ec06d62609458fd7b67f5
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-02 17:43:48 -0500

    Add everything up to post

--- src/components/CommentItem/CommentItem.jsx
+++ src/components/CommentItem/CommentItem.jsx
@@ -1,12 +1,30 @@
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function CommentItem() {
+
+    const [newComment, setNewComment] = useState('')
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const onAddNewComment = (event) => {
+        event.preventDefault();
+        dispatch({
+            type: 'SET_COMMENT',
+            payload: newComment
+        });
+        setNewComment('');
+        history.push('/review');
+    };
 
     return (
 
         <>
             <h2>Any comments you want to leave?</h2>
-            <input type="text" placeholder="Comments" /><button>NEXT</button>
+            <form onSubmit={onAddNewComment}>
+            <input type="text" placeholder="Comments" onChange={event => setNewComment(event.target.value)} value={newComment}/><button>NEXT</button>
+            </form>
         </>
 
 
--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,14 +1,39 @@
 import { useSelector } from 'react-redux';
+import { axios } from 'axios';
+import { useHistory } from 'react-router-dom';
+import { HashRouter as Router, Route, Link} from 'react-router';
 
 function ReviewItem() {
 
-    const feelings = useSelector(store => store.feelingReducer)
+    const feeling = useSelector(store => store.feelingReducer)
 
     const understanding = useSelector(store => store.understandingReducer)
 
     const support = useSelector(store => store.supportedReducer)
 
-    const comment = useSelector(store => store.commentReducer)
+    const comments = useSelector(store => store.commentReducer)
+
+    const handleSubmit = () => {
+
+        const feedback = {
+            feeling: 2,
+            understanding: 2,
+            support: 2,
+            comments: 'thanks'
+
+        };
+        console.log(feedback);
+
+        axios.post('/feedback', feedback )
+        .then(response => {
+            // history.push('/success')
+        }).catch(err => {
+            console.log(err);
+        })
+
+    }
+
+
 
 
 
@@ -16,10 +41,11 @@ function ReviewItem() {
     return (
         <>
             <h2>Review Your Feedback</h2>
-            <p>Feelings: {feelings}</p>
+            <p>Feelings: {feeling}</p>
             <p>Understanding: {understanding}</p>
             <p>Support: {support}</p>
-            <p>Comments: {comment}</p>
+            <p>Comments: {comments}</p>
+            <button onClick={handleSubmit}>SUBMIT</button>
         </>
 
     )
