9c4690a486209b90468f18a2e235b084f48338a8
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-01 19:18:43 -0500

    Add reducers, and dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers} from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const oneReducer = (state = 'oneReducerOutput', action) => {
+    console.log('I am the oneReducer');
+
+  return state;  
+};
+
+const twoReducer = (state = 'twoReducerOutput', action) => {
+    console.log('I am the twoReducer');
+
+  return state;  
+};
+
+const threeReducer = (state = 'threeReducerOutput', action) => {
+    console.log('I am the threeReducer');
+
+  return state;  
+};
+
+const fourReducer = (state = 'fourReducerOutput', action) => {
+    console.log('I am the fourReducer');
+
+  return state;  
+};
+
+const storeInstance = createStore(
+    combineReducers(
+        {
+            oneReducer,
+            twoReducer,
+            threeReducer,
+            fourReducer
+        }
+    )
+);
+
+ReactDOM.render(
+<Provider store={storeInstance}>
+<App />
+</Provider>, 
+document.getElementById('root'));
 registerServiceWorker();
